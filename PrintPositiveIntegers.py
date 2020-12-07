8ce398a8ecb5aa7c67574bfee7510fcd9ac5a803
chinmayckgit <75604037+chinmayckgit@users.noreply.github.com>
2020-12-07 12:34:20 +0530

    Add files via upload

---
+++ PrintPositiveIntegers.py
@@ -0,0 +1,16 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+print("**********List 1**********\n")
+print(list1)
+print("Positive integers in List 1 :\n")
+for i in list1:
+    a=i
+    if a>0:
+        print(a)
+print("**********List 2**********\n")
+print(list2)
+print("Positive integers in List 2 :\n")
+for i in list2:
+    a=i
+    if a>0:
+        print(a)
